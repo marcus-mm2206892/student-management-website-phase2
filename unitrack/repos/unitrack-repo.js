82677426ea34034dd669e5b21cb3c126175f7cee
josh-jc2203604 <joshcalma21@yahoo.com>
2025-05-06 19:36:30 +0300

    Update unitrack-repo.js

--- unitrack/repos/unitrack-repo.js
+++ unitrack/repos/unitrack-repo.js
@@ -8,6 +8,19 @@ class UniTrackRepo {
         return await prisma.course.findMany({ include: { CourseMajorOfferings: true }});
     }
 
+    async getStudents(){
+        return await prisma.student.findMany();
+    }
+
+    async getStudent(id){
+        return await prisma.student.findUnique({where: {studentId: parseInt(id)}});
+    }
+
+    async updateStudent(id, updatedData){
+        return await prisma.student.update({data: updatedData, where: {studentId: parseInt(id)}});
+    }
+    
+    async 
 }
 
 export default new UniTrackRepo();
