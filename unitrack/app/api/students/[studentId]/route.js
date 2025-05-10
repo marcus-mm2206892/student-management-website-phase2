7bbd7e9695bfff27c8a094a14b81f2ea69d2dfef
josh-jc2203604 <joshcalma21@yahoo.com>
2025-05-10 21:22:00 +0300

    student api and new schema

--- unitrack/app/api/students/[studentId]/route.js
+++ unitrack/app/api/students/[studentId]/route.js
@@ -1,11 +1,20 @@
-export async function GET(req){
-    const { searchParams } = new URL(req.url);
-    // const type = searchParams.get('Type');
-    const response = { message: 'API endpoint http://localhost:3000 ' }
-    return Response.json(response, {status: 200});
+import unitrackRepo from "@/repos/unitrack-repo";
+
+export async function GET(req, props){
+    const id = params.studentId;
+    const student = await unitrackRepo.getStudentById(id);
+    
+    if (!student) {
+      return new Response(JSON.stringify({ error: 'Student not found' }), { status: 404 });
+    }
+    
+    return Response.json(student, { status: 200 });
 }
 
 export async function PUT(req){
-    const response = { message: 'API endpoint http://localhost:3000 ' }
-    return Response.json(response, {status: 200});
+    const id = params.studentId;
+    const body = await req.json();
+    const updatedStudent = await unitrackRepo.updateStudent(id, body);
+
+    return Response.json(updatedStudent, { status: 200 });
 }
