4e96f4cf368e1ab6d4e99c943329c5bed0c3cb9d
bashar <muhtasim2k2@gmail.com>
2025-05-12 23:04:16 +0300

    populated prerequisites dropdown

--- unitrack/app/admin/create-course/page.jsx
+++ unitrack/app/admin/create-course/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import styles from "@/app/styles/admin-create-course.module.css";
-import { createPrerequisiteAction, createCourseMajorOfferingAction, createCourseAction, checkCourseIdAction, getAllSubjectCodeAction } from "@/app/action/server-actions";
+import { createPrerequisiteAction, createCourseMajorOfferingAction, createCourseAction, checkCourseIdAction, getAllSubjectCodeAction, getAllCourseIdsAction } from "@/app/action/server-actions";
 import AlertModal from "@/app/components/AlertModal";
 /*
   To be fixed:
@@ -15,6 +15,7 @@ export default function AdminCreateCourse() {
   const [alertVisible, setAlertVisible] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [subjects, setSubjects] = useState([]);
+  const [prerequisites, setPrerequisites] = useState([]);
   const [selectedSubject, setSelectedSubject] = useState("Select Subject");
   const [selectedPrereqs, setSelectedPrereqs] = useState([]);
   const [selectedMajors, setSelectedMajors] = useState([]);
@@ -28,7 +29,7 @@ export default function AdminCreateCourse() {
 
 
   // const subjects = ["CMPS", "CMPE", "ELEC"];
-  const prerequisites = ["CMPS101", "CMPS202", "CMPS303"];
+  // const prerequisites = ["CMPS101", "CMPS202", "CMPS303"];
   const majors = ["Computer Science", "Computer Engineering"];
 
 
@@ -38,6 +39,14 @@ export default function AdminCreateCourse() {
       setSubjects(codes);
     }
     fetchSubjects();
+  }, []);
+
+  useEffect(() => {
+  async function fetchPrereqs() {
+      const courseIds = await getAllCourseIdsAction();
+      setPrerequisites(courseIds);
+    }
+    fetchPrereqs();
   }, []);
 
   const handleMultiSelect = (item, list, setter) => {
